857e62261aead6e2190cc6e1e7aad33a23f93a96
alonbarak-dev <alonbarak3110@gmail.com>
2023-04-20 06:25:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/opticalflow/KLT.java
@@ -0,0 +1,155 @@
+package com.example.opticalflow;
+
+import android.util.Log;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.video.Video;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class KLT {
+
+    private Mat prevFrame, currFrame, prevGray, currGray, MVframe;
+    private Mat[] output;
+    private MatOfPoint2f prevPts, currPts;
+    private MatOfByte status;
+    private byte[] status_array;
+    private Point[] prevPts_arr, currPts_arr;
+    private MatOfFloat err;
+    private Scalar color;
+    private int flow_pts;
+    private boolean update_features;
+    private Point prevMv, currMv;
+    private double x_avg1, x_avg2, y_avg1, y_avg2;
+    Queue<Mat> in_frames, out_frames;
+
+
+    public KLT(){
+        prevFrame = new Mat();
+        currFrame = new Mat();
+        prevGray = new Mat();
+        currGray = new Mat();
+        output = new Mat[2];
+        prevPts = new MatOfPoint2f();
+        currPts = new MatOfPoint2f();
+        status = new MatOfByte();
+        err = new MatOfFloat();
+        color = new Scalar(240,230,140);
+        flow_pts = 500;
+        MVframe = new Mat(400,400, CvType.CV_8UC1);
+        update_features = false;
+    }
+
+
+    public void reset_motion_vector(){
+        MVframe = new Mat(400,400, CvType.CV_8UC1);
+        prevMv = null;
+        currMv = null;
+    }
+
+    public void UpdateFeatures(){
+        this.update_features = true;
+    }
+    public void update_points(Mat prevGray, Mat currGray, MatOfPoint2f prevPts){
+
+        currGray.copyTo(prevGray);
+        // detect features in the first frame
+        MatOfPoint corners = new MatOfPoint();
+        Imgproc.goodFeaturesToTrack(prevGray, corners, 500, 0.1, 5);
+        prevPts.fromArray(corners.toArray());
+    }
+
+    public Mat[] run(Mat new_frame){
+        // init
+        Log.d("RUN-OF", "started");
+        currFrame = new_frame;
+
+        // convert the frame to Gray
+        Imgproc.cvtColor(currFrame, currGray, Imgproc.COLOR_RGBA2GRAY);
+
+        // if this is the first loop, find good features
+        if (prevGray.empty()){
+            this.update_points(prevGray, currGray, prevPts);
+            output[0] = null;
+            output[1] = null;
+            return output;
+        }
+        if (flow_pts < 300 || this.update_features){
+            this.update_points(prevGray, currGray, prevPts);
+            this.update_features = false;
+        }
+
+        // Run the KLT algorithm for Optical Flow
+        Video.calcOpticalFlowPyrLK(prevGray, currGray, prevPts, currPts, status, err);
+
+        // draw the flow vectors
+        flow_pts = 0;
+        x_avg1 = 0;
+        x_avg2 = 0;
+        y_avg2 = 0;
+        y_avg1 = 0;
+        status_array = status.toArray();
+        prevPts_arr = prevPts.toArray();
+        currPts_arr = currPts.toArray();
+        for (int i =0; i < prevPts.rows(); i++){
+            if (status_array[i] == 1){
+                Point pt1 = new Point(prevPts_arr[i].x, prevPts_arr[i].y);
+                Point pt2 = new Point(currPts_arr[i].x, currPts_arr[i].y);
+                x_avg1 += pt1.x;
+                x_avg2 += pt2.x;
+                y_avg1 += pt1.y;
+                y_avg2 += pt2.y;
+                Imgproc.line(currFrame, pt1, pt2, color, 10);
+                flow_pts++;
+            }
+        }
+
+        // Calculate the motion vector
+        x_avg1 /= flow_pts;
+        y_avg1 /= flow_pts;
+        x_avg2 /= flow_pts;
+        y_avg2 /= flow_pts;
+
+        currMv = new Point(x_avg1 - x_avg2, y_avg1 - y_avg2);
+        if (prevMv == null){
+            currMv.x += 200;
+            currMv.y += 200;
+            prevMv = currMv;
+        }
+        else{
+            currMv.x += prevMv.x;
+            currMv.y += prevMv.y;
+            Imgproc.line(MVframe, prevMv, currMv, color, 4);
+        }
+
+        Log.d("RUN-OF", "Processed");
+        // update variables for next iteration
+        currGray.copyTo(prevGray);
+        prevPts.fromArray(currPts.toArray());
+        output[0] = currFrame;
+        output[1] = MVframe;
+        return output;
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/opticalflow/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.opticalflow;
+
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2, View.OnClickListener {
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    static {
+        // load OpenCV
+        if (OpenCVLoader.initDebug()){
+            Log.d("OpenCV", "success");
+        }
+        else{
+            Log.d("OpenCV", "failed");
+        }
+    }
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private ImageView motionVector;
+    private Button reset_button, update_features_button;
+    private Mat curr_frame;
+    private Mat[] output_klt;
+    private KLT optical_flow;
+
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this)
+    {
+        @Override
+        public void onManagerConnected(int status)
+        {
+            switch (status)
+            {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully");
+
+                    mOpenCvCameraView.enableView();
+
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_layout);
+        // KLT
+        optical_flow = new KLT();
+        output_klt = new Mat[2];
+        // reset Button
+        reset_button = (Button)findViewById(R.id.resetMV);
+        reset_button.setOnClickListener(this);
+        // update features Button
+        update_features_button = (Button)findViewById(R.id.update_features_button);
+        update_features_button.setOnClickListener(this);
+        // Image view
+        motionVector = (ImageView) findViewById(R.id.motion_vector);
+        motionVector.setVisibility(View.VISIBLE);
+        // Java view
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        ActivityCompat.shouldShowRequestPermissionRationale(this,
+                android.Manifest.permission.CAMERA);    // permission
+        mOpenCvCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_BACK);
+        mOpenCvCameraView.setCameraPermissionGranted();
+        mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug())
+        {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+                    OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this,
+                            mLoaderCallback);
+        }
+        else
+        {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+    @Override
+    public void onCameraViewStarted(int width, int height)
+    {
+        Log.d(TAG, "onCameraViewStarted");
+    }
+
+    @Override
+    public void onCameraViewStopped()
+    {
+        Log.d(TAG, "onCameraViewStopped");
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame)
+    {
+        curr_frame = inputFrame.rgba();
+        output_klt = optical_flow.run(curr_frame);
+        if (output_klt[0] != null) {
+            if (output_klt[1] != null) {
+                Bitmap dst = Bitmap.createBitmap(output_klt[1].width(), output_klt[1].height(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(output_klt[1], dst);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        motionVector.setImageBitmap(dst);
+                    }
+                });
+            }
+            return output_klt[0];
+        }
+        return inputFrame.rgba();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.resetMV:
+                optical_flow.reset_motion_vector();
+                break;
+            case R.id.update_features_button:
+                optical_flow.UpdateFeatures();
+                break;
+            default:
+                break;
+        }
+    }
+}
